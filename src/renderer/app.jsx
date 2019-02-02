52337938666341aa0c988f2b28ac15d59bc87d38
Pat Higgins <patrick@gremlininc.com>
2019-02-02 12:15:20 -0800

    photonkit successfully integrated

--- src/renderer/app.jsx
+++ src/renderer/app.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import {Window, Toolbar, Content} from 'react-photonkit';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Welcome to React</h2>
-      </div>
+      <Window>
+        <Content>
+          <table className="table-striped">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Kind</th>
+                <th>File Size</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>photon.css</td>
+                <td>CSS</td>
+                <td>28K</td>
+              </tr>
+              <tr>
+                <td>photon.css</td>
+                <td>CSS</td>
+                <td>28K</td>
+              </tr>
+              <tr>
+                <td>photon.css</td>
+                <td>CSS</td>
+                <td>28K</td>
+              </tr>
+              <tr>
+                <td>photon.css</td>
+                <td>CSS</td>
+                <td>28K</td>
+              </tr>
+            </tbody>
+          </table>
+        </Content>
+      </Window>
     );
   }
 }
